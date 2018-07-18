61e20a364fc0f002a5c146cfe830a8094325bc9e
Bot <mrtechtvebots>
2018-07-18 14:41:31 -0400

    Updated birb command to use RichEmbed

--- commands/birb.js
+++ commands/birb.js
@@ -1,11 +1,16 @@
 const birb = require('birb');
+var Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'birb',
 	execute(message) {
         birb.random()
         .then(url => {
-            message.channel.send({ files: [ `${url}`] });
+            const embed = new Discord.MessageEmbed()
+            .setDescription(':bird:')
+            .setColor(0xDD2E44)
+            .setImage(`${url}`)
+        message.channel.send({embed: embed});
         });
 	},
 };
